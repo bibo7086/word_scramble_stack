54e28c8840d4fff5c2c9e83d25d7136cb623cba1
Saeed Ali <S@mac.local>
2016-05-01 12:40:27 +0300

    Initial Commit

---
+++ word_scramble_stack/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  word_scramble_stack
+//
+//  Created by Saeed Ali on 01/05/2016.
+//  Copyright © 2016 Saeed Ali. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
